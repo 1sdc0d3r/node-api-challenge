37b90aff04740070dc7fdcbbaccaf45cda26df8a
Braden <fish2355@hotmail.com>
2020-02-14 10:12:34 -0700

    finished stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  NavLink,
+  Link,
+  Route
+} from "react-router-dom";
+import ProjectList from "./components/ProjectList";
+import Project from "./components/Project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <NavLink to="/projects">Projects</NavLink>
+      </nav>
+      <Switch>
+        <Route path="/projects/:id" component={Project} />
+        <Route path="/projects" component={ProjectList} />
+      </Switch>
+    </Router>
   );
 }
 
